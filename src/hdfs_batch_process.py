92e3659d75e5a3f32ebfdc5a68ff3a45eb8fac7b
Emmanuel Awa <eawa@brandeis.edu>
2016-01-20 15:22:31 -0500

    Adding dataframes for nitelife, products and events

--- src/hdfs_batch_process.py
+++ src/hdfs_batch_process.py
@@ -9,5 +9,15 @@ from pyspark import SparkContext
 sc = SparkContext()
 sqlContext = sqlcon(sc)
 
-df = sqlContext.read.json('/tmp/exstreamly_cheap_files/merchants.json')
-df.printSchema()
+df_merchants = sqlContext.read.json('/tmp/exstreamly_cheap_files/merchants.json')
+df_merchants.printSchema()
+
+df_events = sqlContext.read.json('/tmp/exstreamly_cheap_files/activities-events.json')
+df_events.printSchema()
+
+df_nitelife = sqlContext.read.json('/tmp/exstreamly_cheap_files/dining-nightlife.json')
+df_nitelife.printSchema()
+
+df_products = sqlContext.read.json('/tmp/exstreamly_cheap_files/product.json')
+df_products.printSchema()
+
