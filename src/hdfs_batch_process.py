ab41c5d358e936c61cb58a3fa5b407487a70a696
Emmanuel Awa <eawa@brandeis.edu>
2016-01-20 18:57:02 -0500

    WIP

--- src/hdfs_batch_process.py
+++ src/hdfs_batch_process.py
@@ -5,19 +5,53 @@
 '''
 from pyspark.sql import SQLContext as sqlcon
 from pyspark import SparkContext
+from helper_modules import utility_functions as uf
 
 sc = SparkContext()
 sqlContext = sqlcon(sc)
 
-df_merchants = sqlContext.read.json('/tmp/exstreamly_cheap_files/merchants.json')
-df_merchants.show()
+#df_merchants = sqlContext.read.json('/tmp/exstreamly_cheap_files/merchants.json')
+#df_merchants.show()
+#
+#df_events = sqlContext.read.json('/tmp/exstreamly_cheap_files/activities-events.json')
+#df_events.printSchema()
+#
+#df_nitelife = sqlContext.read.json('/tmp/exstreamly_cheap_files/dining-nightlife.json')
+#df_nitelife.printSchema()
+#
+#df_products = sqlContext.read.json('/tmp/exstreamly_cheap_files/product.json')
+#df_products.printSchema()
 
-df_events = sqlContext.read.json('/tmp/exstreamly_cheap_files/activities-events.json')
-df_events.printSchema()
+def create_dataframe(json_filepath):
+    ''' Read in a json file and return a dataframe '''
+    return sqlContext.read.json(json_filepath)
 
-df_nitelife = sqlContext.read.json('/tmp/exstreamly_cheap_files/dining-nightlife.json')
-df_nitelife.printSchema()
+def remove_duplicate_deals(df):
+    ''' Return new dataframe with distinct records '''
+    return df.distinct()
 
-df_products = sqlContext.read.json('/tmp/exstreamly_cheap_files/product.json')
-df_products.printSchema()
+def count_unique_rows(clean_df):
+    ''' Return number of unique deals in a category '''
+    return clean_df.count()
+
+if __name__ == '__main__':
+    uf.print_out('Cleaning Merchants Table.')
+    df_merchants = create_dataframe('/tmp/exstreamly_cheap_files/merchants.json')
+    unique_merchants = count_unique_rows(df_merchants)
+    uf.print_out('Number of unique merchants serving deals: {}'.format(unique_merchants))
+    
+    uf.print_out('Cleaning Dining and Nightlife Table.')
+    df_nitelife = create_dataframe('/tmp/exstreamly_cheap_files/dining-nightlife.json')
+    unique_nitelife = count_unique_rows(df_nitelife)
+    uf.print_out('Number of unique Dining and Nightlife deals: {}'.format(unique_nitelife))
+    
+    uf.print_out('Cleaning Activities and Events Table.')
+    df_events = create_dataframe('/tmp/exstreamly_cheap_files/activities-events.json')
+    unique_events = count_unique_rows(df_events)
+    uf.print_out('Number of unique Activities and Events deals: {}'.format(unique_events))
+    
+    uf.print_out('Cleaning Products Table.')
+    df_products = create_dataframe('/tmp/exstreamly_cheap_files/product.json')
+    unique_products = count_unique_rows(df_products)
+    uf.print_out('Number of unique Product deals: {}'.format(unique_products))
 
