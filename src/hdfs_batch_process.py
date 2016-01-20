4d153cdaf38971401e7af134d9b35914a22a0202
Emmanuel Awa <eawa@brandeis.edu>
2016-01-20 14:59:00 -0500

    Import Spark Context

--- src/hdfs_batch_process.py
+++ src/hdfs_batch_process.py
@@ -4,6 +4,9 @@
     @Author: Emmanuel Awa
 '''
 from pyspark.sql import SQLContext as sqlcon
+from pyspark import SparkContext
+
+sc = SparkContext()
 sqlContext = sqlcon(sc)
 
 df = sqlContext.read.json('/tmp/exstreamly_cheap_files/merchants.json')
