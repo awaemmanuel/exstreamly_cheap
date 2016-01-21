be53a16d5a3493c9cbb7bc507bd808b755d7f00d
Emmanuel Awa <eawa@brandeis.edu>
2016-01-21 15:02:34 -0500

    Generalizing the batch process

--- src/hdfs_batch_process.py
+++ src/hdfs_batch_process.py
@@ -29,7 +29,7 @@ def count_unique_rows(clean_df):
 if __name__ == '__main__':
     categories = ['merchants', 'dining-nightlife', 'activities-events', 'product']
     for category in categories:
-        uf.print_out('Cleaning {}.capitalize() Table.'.format(category))
+        uf.print_out('Cleaning {} Table.'.format(category.capitalize()))
         df_category = create_dataframe('/tmp/exstreamly_cheap_files/{}.json'.format(category))
         df_category = remove_duplicate_deals(df_category)
         unique_vals = count_unique_rows(df_category)
