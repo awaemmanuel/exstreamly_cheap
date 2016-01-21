12c035b438cb580bbe0507cdd92f8d272714b8cd
Emmanuel Awa <eawa@brandeis.edu>
2016-01-21 15:42:16 -0500

    WIP

--- src/hdfs_batch_process.py
+++ src/hdfs_batch_process.py
@@ -34,28 +34,3 @@ if __name__ == '__main__':
         df_category = remove_duplicate_deals(df_category)
         unique_vals = count_unique_rows(df_category)
         uf.print_out('Number of unique {} serving deals: {}'.format(category, unique_vals))
-    
-#    uf.print_out('Cleaning Merchants Table.')
-#    df_merchants = create_dataframe('/tmp/exstreamly_cheap_files/merchants.json')
-#    df_merchants = remove_duplicate_deals(df_merchants)
-#    unique_merchants = count_unique_rows(df_merchants)
-#    uf.print_out('Number of unique merchants serving deals: {}'.format(unique_merchants))
-#    
-#    uf.print_out('Cleaning Dining and Nightlife Table.')
-#    df_nitelife = create_dataframe('/tmp/exstreamly_cheap_files/dining-nightlife.json')
-#    df_nitelife = remove_duplicate_deals(df_nitelife)
-#    unique_nitelife = count_unique_rows(df_nitelife)
-#    uf.print_out('Number of unique Dining and Nightlife deals: {}'.format(unique_nitelife))
-#    
-#    uf.print_out('Cleaning Activities and Events Table.')
-#    df_events = create_dataframe('/tmp/exstreamly_cheap_files/activities-events.json')
-#    df_events = remove_duplicate_deals(df_events)
-#    unique_events = count_unique_rows(df_events)
-#    uf.print_out('Number of unique Activities and Events deals: {}'.format(unique_events))
-#    
-#    uf.print_out('Cleaning Products Table.')
-#    df_products = create_dataframe('/tmp/exstreamly_cheap_files/product.json')
-#    df_products = remove_duplicate_deals(df_products)
-#    unique_products = count_unique_rows(df_products)
-#    uf.print_out('Number of unique Product deals: {}'.format(unique_products))
-#
