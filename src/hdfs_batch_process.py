6499e50407dda2bd0b04d316418261634305f527
Emmanuel Awa <eawa@brandeis.edu>
2016-01-20 19:04:50 -0500

    WIP

--- src/hdfs_batch_process.py
+++ src/hdfs_batch_process.py
@@ -10,18 +10,6 @@ from helper_modules import utility_functions as uf
 sc = SparkContext()
 sqlContext = sqlcon(sc)
 
-#df_merchants = sqlContext.read.json('/tmp/exstreamly_cheap_files/merchants.json')
-#df_merchants.show()
-#
-#df_events = sqlContext.read.json('/tmp/exstreamly_cheap_files/activities-events.json')
-#df_events.printSchema()
-#
-#df_nitelife = sqlContext.read.json('/tmp/exstreamly_cheap_files/dining-nightlife.json')
-#df_nitelife.printSchema()
-#
-#df_products = sqlContext.read.json('/tmp/exstreamly_cheap_files/product.json')
-#df_products.printSchema()
-
 def create_dataframe(json_filepath):
     ''' Read in a json file and return a dataframe '''
     return sqlContext.read.json(json_filepath)
@@ -37,21 +25,25 @@ def count_unique_rows(clean_df):
 if __name__ == '__main__':
     uf.print_out('Cleaning Merchants Table.')
     df_merchants = create_dataframe('/tmp/exstreamly_cheap_files/merchants.json')
+    df_merchants = remove_duplicate_deals(df_merchants)
     unique_merchants = count_unique_rows(df_merchants)
     uf.print_out('Number of unique merchants serving deals: {}'.format(unique_merchants))
     
     uf.print_out('Cleaning Dining and Nightlife Table.')
     df_nitelife = create_dataframe('/tmp/exstreamly_cheap_files/dining-nightlife.json')
+    df_nitelife = remove_duplicate_deals(df_nitelife)
     unique_nitelife = count_unique_rows(df_nitelife)
     uf.print_out('Number of unique Dining and Nightlife deals: {}'.format(unique_nitelife))
     
     uf.print_out('Cleaning Activities and Events Table.')
     df_events = create_dataframe('/tmp/exstreamly_cheap_files/activities-events.json')
+    df_events = remove_duplicate_deals(df_events)
     unique_events = count_unique_rows(df_events)
     uf.print_out('Number of unique Activities and Events deals: {}'.format(unique_events))
     
     uf.print_out('Cleaning Products Table.')
     df_products = create_dataframe('/tmp/exstreamly_cheap_files/product.json')
+    df_products = remove_duplicate_deals(df_products)
     unique_products = count_unique_rows(df_products)
     uf.print_out('Number of unique Product deals: {}'.format(unique_products))
 
