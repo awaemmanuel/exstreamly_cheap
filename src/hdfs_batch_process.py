3090a29bc20d2f6702ba9c2b2350225b648380a7
Emmanuel Awa <eawa@brandeis.edu>
2016-01-21 15:00:04 -0500

    Generalizing the batch process

--- src/hdfs_batch_process.py
+++ src/hdfs_batch_process.py
@@ -22,7 +22,19 @@ def count_unique_rows(clean_df):
     ''' Return number of unique deals in a category '''
     return clean_df.count()
 
+#def batch_process_data(data):
+#    ''' Call all helper functions to batch process files '''
+#    file_name = data[0]
+
 if __name__ == '__main__':
+    categories = ['merchants', 'dining-nightlife', 'activities-events', 'product']
+    for category in categories:
+        uf.print_out('Cleaning {}.capitalize() Table.'.format(category))
+        df_category = create_dataframe('/tmp/exstreamly_cheap_files/{}.json'.format(category))
+        df_category = remove_duplicate_deals(df_category)
+        unique_vals = count_unique_rows(df_category)
+        uf.print_out('Number of unique {} serving deals: {}'.format(category, unique_vals))
+    
     uf.print_out('Cleaning Merchants Table.')
     df_merchants = create_dataframe('/tmp/exstreamly_cheap_files/merchants.json')
     df_merchants = remove_duplicate_deals(df_merchants)
