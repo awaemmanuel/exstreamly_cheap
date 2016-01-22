606001485cfe044ec2345cd85ca803d87a073831
Emmanuel Awa <eawa@brandeis.edu>
2016-01-22 11:54:49 -0500

    Appending DataFrame of categories from spark to list for further processing

--- src/database_files/insert_into_database.py
+++ src/database_files/insert_into_database.py
@@ -4,9 +4,16 @@
 '''
 from src.helper_modules import utility_functions as uf
 from src.batch_process import hdfs_batch_process as hbp
+from create_database_schema import *
 
+# Constants definition
+INPUT_KEY_CONVERTER = "com.parsely.spark.converters.FromUsersCQLKeyConverter"
+INPUT_VALUE_CONVERTER = "com.parsely.spark.converters.FromUsersCQLValueConverter"
+OUTPUT_KEY_CONVERTER = "com.parsely.spark.converters.ToCassandraCQLKeyConverter"
+OUTPUT_VALUE_CONVERTER = "com.parsely.spark.converters.ToCassandraCQLValueConverter"
 
 if __name__ == '__main__':
+    results = []
     categories = ['merchants', 'dining-nightlife', 'activities-events', 'product']
     for category in categories:
         uf.print_out('Cleaning {} Table.'.format(category.capitalize()))
@@ -14,3 +21,10 @@ if __name__ == '__main__':
         df_category = hbp.remove_duplicate_deals(df_category)
         unique_vals = hbp.count_unique_rows(df_category)
         uf.print_out('Number of unique {} serving deals: {}'.format(category, unique_vals))
+        results.append(df_category)
+        
+    # DataFrames with all our categories
+    print results
+        
+    
+    
