d5c8662a1edc978cd2c203e7840b1f96e37e3f2f
Emmanuel Awa <awaemmanuel1@gmail.com>
2016-01-22 16:50:01 +0000

    Fixed referencing issues with packages and modules

--- src/database_files/insert_into_database.py
+++ src/database_files/insert_into_database.py
@@ -2,15 +2,15 @@
     Module that helps insert data from spark to database
     @Author: Emmanuel Awa
 '''
-from helper_modules import utility_functions as uf
-from batch_process import *
+from src.helper_modules import utility_functions as uf
+from src.batch_process import hdfs_batch_process as hbp
 
 
 if __name__ == '__main__':
     categories = ['merchants', 'dining-nightlife', 'activities-events', 'product']
     for category in categories:
         uf.print_out('Cleaning {} Table.'.format(category.capitalize()))
-        df_category = create_dataframe('/tmp/exstreamly_cheap_files/{}.json'.format(category))
-        df_category = remove_duplicate_deals(df_category)
-        unique_vals = count_unique_rows(df_category)
+        df_category = hbp.create_dataframe('/tmp/exstreamly_cheap_files/{}.json'.format(category))
+        df_category = hbp.remove_duplicate_deals(df_category)
+        unique_vals = hbp.count_unique_rows(df_category)
         uf.print_out('Number of unique {} serving deals: {}'.format(category, unique_vals))
