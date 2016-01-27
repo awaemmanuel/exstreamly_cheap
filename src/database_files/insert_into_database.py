4b32526345214d5dac8dd5295bb6b3435527ee3d
Emmanuel Awa <awaemmanuel1@gmail.com>
2016-01-27 21:16:31 +0000

    Reading files from hdfs

--- src/database_files/insert_into_database.py
+++ src/database_files/insert_into_database.py
@@ -19,7 +19,9 @@ if __name__ == '__main__':
     categories = ['merchants', 'dining_nightlife', 'activities_events', 'products']
     for category in categories:
         uf.print_out('Cleaning {} Table.'.format(category.capitalize()))
-        df_category = hbp.create_dataframe('/tmp/exstreamly_cheap_files/{}.json'.format(category))
+        file_name = 'hdfs://52.1.154.19:9000/exstreamly_cheap_files/exstreamly_cheap_files/{}.json'.format(category)
+        print file_name
+        df_category = hbp.create_dataframe(file_name)
         df_category = hbp.remove_duplicate_deals(df_category)
         unique_vals = hbp.count_unique_rows(df_category)
         uf.print_out('Number of unique {} serving deals: {}'.format(category, unique_vals))
