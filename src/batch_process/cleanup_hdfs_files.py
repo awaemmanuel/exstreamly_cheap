f5cafc62d353123cb3218c749fc788925c9bd77a
Emmanuel Awa <awaemmanuel1@gmail.com>
2016-02-14 00:34:02 +0000

    Batch process of a single file

--- src/batch_process/cleanup_hdfs_files.py
+++ src/batch_process/cleanup_hdfs_files.py
@@ -5,17 +5,40 @@
     from deals description, and then resaves it as proper json.
 '''
 from pyspark import SparkContext, SparkConf
-from hdfs_batch_process import evaluate_and_purify as eap
+from pyspark.sql import SQLContext
+from collections import OrderedDict
 
-appName = 'CleanUpHDFSFiles'
-master = '172.31.2.36'
-conf = SparkConf().setAppName(appName).setMaster(master)
-sc = SparkContext(conf=conf)
-base_uri = 'hdfs://52.1.154.19:9000/exstreamly_cheap_main_files/all_deals/history'
+def evaluate_and_purify(msg):
+    ''' Evaluate string as literal,
+        if msg is a stringified python object
+        return object else fail gracefully.
 
-# Create external dataset
-distFile = sc.textFile('{}/deals_data_hdfs_all_deals_data_20160205051834.dat'.format(base_uri))
+        :args: msg - Possible stringified python object
+        :return: cleaned msg - python dict
+    '''
+    try:
+        print "Inside Try block\n"
+        msg = eval(msg.encode('utf-8'))
+        if isinstance(msg, OrderedDict):
+            print "Inside isinstance\n"
+            return dict(msg)
+        return msg
+    except SyntaxError:
+        print "Inside Exception\n"
+        raise
+if __name__ == '__main__':
 
-distFile = distFile.map(eap)
+    appName = 'CleanUpHDFSFiles'
+    master = 'spark://ip-172-31-2-36:7077'
+    conf = SparkConf().setAppName(appName).setMaster(master)
+    sc = SparkContext(conf=conf)
+    sqlContext = SQLContext(sc)
+    base_uri = 'hdfs://52.1.154.19:9000/exstreamly_cheap_main_files/all_deals/history'
 
-print "Done"
+    # Create external dataset
+    distFile = sc.textFile('{}/deals_data_hdfs_all_deals_data_20160205051834.dat'.format(base_uri))
+    distFile = distFile.map(evaluate_and_purify).toDF()
+    distFile.printSchema()
+    distFile.show()
+    #distFile.pprint()
+    print "Done"
